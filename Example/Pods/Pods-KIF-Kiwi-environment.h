5342c3ff9629b32349917f579a7e0f88a77ec8cf
Glen Tregoning & Paul Zabelin <pair+glen.tregoning+paul@indiegogo.com>
2014-09-24 11:43:43 -0700

    add workaround for cocoapods issue https://github.com/CocoaPods/CocoaPods/issues/2516 - Podfile with no pods in one of targets cause compilation failure

---
+++ Example/Pods/Pods-KIF-Kiwi-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// KIF
+#define COCOAPODS_POD_AVAILABLE_KIF
+#define COCOAPODS_VERSION_MAJOR_KIF 3
+#define COCOAPODS_VERSION_MINOR_KIF 0
+#define COCOAPODS_VERSION_PATCH_KIF 8
+
+// KIF/XCTest
+#define COCOAPODS_POD_AVAILABLE_KIF_XCTest
+#define COCOAPODS_VERSION_MAJOR_KIF_XCTest 3
+#define COCOAPODS_VERSION_MINOR_KIF_XCTest 0
+#define COCOAPODS_VERSION_PATCH_KIF_XCTest 8
+
+// KIF-Kiwi
+#define COCOAPODS_POD_AVAILABLE_KIF_Kiwi
+#define COCOAPODS_VERSION_MAJOR_KIF_Kiwi 0
+#define COCOAPODS_VERSION_MINOR_KIF_Kiwi 2
+#define COCOAPODS_VERSION_PATCH_KIF_Kiwi 1
+
+// Kiwi
+#define COCOAPODS_POD_AVAILABLE_Kiwi
+#define COCOAPODS_VERSION_MAJOR_Kiwi 2
+#define COCOAPODS_VERSION_MINOR_Kiwi 3
+#define COCOAPODS_VERSION_PATCH_Kiwi 0
+
